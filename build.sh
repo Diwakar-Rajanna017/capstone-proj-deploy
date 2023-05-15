a041d34c52764799a93d7c588a8cfba892d0181f
Diwakar <diwakar.rajanna@gmail.com>
2023-05-15 11:36:16 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TAG=${1:-"latest"}
+
+if [ -z "$1" ]; then
+    echo "No version number provided. Using 'latest' as the default tag."
+fi
+
+IMAGE_NAME="my-capstone-app"
+
+echo "Building Docker image ${IMAGE_NAME}:${TAG}"
+
+docker build -t "${IMAGE_NAME}:${TAG}" .
+
+if [ "$?" -eq 0 ]; then
+    echo "Docker image ${IMAGE_NAME}:${TAG} built successfully."
+else
+    echo "Error: Docker image build failed."
+    exit 1
+fi
+
+echo "List of Docker images:"
+docker images
